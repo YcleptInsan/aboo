78bc439b6992eddf73de8970942c367398b990fe
YcleptInsan <nickmwise@gmail.com>
2017-04-25 13:24:59 -0600

    add 2 flash cards with toggle feature. need hide show feature for ul items.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#flash1").click(function() {
+    $(".flash1-cont").slideToggle(1000);
+
+  });
+  $("#flash2").click(function() {
+  $(".flash2-cont").slideToggle(1000);
+
+  });
+  $("#togglelist").click(function() {
+  $(".assignment-list").slideToggle(1000);
+
+  });
+  $("#flash3").click(function() {
+    $(".flash3-cont").slideToggle(1000);
+
+  });
+
+  $("#flash4").click(function() {
+    $(".flash4-cont").slideToggle(1000);
+
+  });
+  $("#flash5").click(function() {
+    $(".flash5-cont").slideToggle(1000);
+
+  });
+  $("#flash6").click(function() {
+    $(".flash6-cont").slideToggle(1000);
+
+  });
+  $("#flash7").click(function() {
+    $(".flash7-cont").slideToggle(1000);
+
+  });
+});
