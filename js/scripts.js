d9a9462135b607bfd377f1daa8da16e3d4dc43d8
Jun Fritz and Nick Wise <jun+nick@>
2017-04-25 16:42:02 -0700

    comment out unused code.

--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,30 @@ $(document).ready(function() {
   });
   $(".flash2-cont").click(function() {
   $(".first-list").slideToggle(1000);
-  $("#flash2").eventPause("active").click();
   })
   $("#togglelist").click(function() {
   $(".assignment").slideToggle(1000);
 
   });
-  $("#flash3").click(function() {
-    $(".flash3-cont").slideToggle(1000);
-
-  });
-
-  $("#flash4").click(function() {
-    $(".flash4-cont").slideToggle(1000);
-
-  });
-  $("#flash5").click(function() {
-    $(".flash5-cont").slideToggle(1000);
-
-  });
-  $("#flash6").click(function() {
-    $(".flash6-cont").slideToggle(1000);
-
-  });
-  $("#flash7").click(function() {
-    $(".flash7-cont").slideToggle(1000);
-
-  });
+  // $("#flash3").click(function() {
+  //   $(".flash3-cont").slideToggle(1000);
+  //
+  // });
+  //
+  // $("#flash4").click(function() {
+  //   $(".flash4-cont").slideToggle(1000);
+  //
+  // });
+  // $("#flash5").click(function() {
+  //   $(".flash5-cont").slideToggle(1000);
+  //
+  // });
+  // $("#flash6").click(function() {
+  //   $(".flash6-cont").slideToggle(1000);
+  //
+  // });
+  // $("#flash7").click(function() {
+  //   $(".flash7-cont").slideToggle(1000);
+  //
+  // });
 });
